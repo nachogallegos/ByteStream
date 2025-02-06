fca6d5cfae0c1a85b60f2ea3692197da73c62ee8
nachogallegosgit config --global user.name nachogallegosgit config --global user.email nachogallegosvargas@gmail.com <nachogallegosvargas@gmail.com>
2025-02-06 02:26:48 -0300

    update para meter datos a bq

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from spotify import get_access_token, get_artist_data
 from storage import save_to_cloud_storage
 from google.oauth2 import service_account
-from google.cloud import storage
+from google.cloud import storage, bigquery
 import os
 
 # Función principal que se ejecutará en Cloud Functions
@@ -16,15 +16,14 @@ def extract_data(request):
 
         print(f"Access Token obtenido: {access_token}")
 
-        # Obtener información del artista (por ejemplo, Marcianeke)
-        artist_id = '5XQWXnMwsvuvCPMneXUbsy'  # ID de Marcianeke
-        artist_data = get_artist_data(access_token, artist_id)
+        # Lista de IDs de artistas
+        artist_ids = ['5XQWXnMwsvuvCPMneXUbsy', '1Yj5Xey7kTwvZla8sqdsdE']  # Marcianeke y Cris Mj
+        artist_data_all = []
 
-        if not artist_data:
-            print("No se pudo obtener la información del artista.")
-            return "Error fetching artist data", 500
-
-        print(f"Datos del artista obtenidos: {artist_data['name']}")
+        for artist_id in artist_ids:
+            artist_data = get_artist_data(access_token, artist_id)
+            if artist_data:
+                artist_data_all.append(artist_data)
 
         # Descargar el archivo de credenciales desde Google Cloud Storage
         storage_client = storage.Client()
@@ -41,11 +40,40 @@ def extract_data(request):
 
         print("Subiendo los datos a Google Cloud Storage...")
         # Guardar los datos en Google Cloud Storage
-        save_to_cloud_storage(artist_data, 'bytestream-data', 'spotify_artist_data.json', storage_client)
+        save_to_cloud_storage(artist_data_all, 'bytestream-data', 'spotify_artists_data.json', storage_client)
 
         print("Datos guardados en Google Cloud Storage correctamente.")
-        return 'Data extracted and saved to Cloud Storage'
-    
+
+        # Crear el cliente de BigQuery con las credenciales
+        bq_client = bigquery.Client(credentials=credentials, project="bytestream-450003")
+        
+        # Definir el dataset y la tabla en BigQuery
+        dataset_id = 'bytestream-450003.spotify_data'
+        table_id = 'bytestream-450003.spotify_data.spotify_artists_data'  # ID completo de la tabla
+
+        # Convertir los datos de los artistas a un formato adecuado para BigQuery
+        rows_to_insert = [
+            {
+                "artist_id": artist["id"],
+                "artist_name": artist["name"],
+                "followers": artist["followers"],
+                "genres": artist["genres"],
+                "images": artist["images"] if isinstance(artist["images"], list) else [],  # Asegúrate de que sea un array de strings,
+                "popularity": artist["popularity"]
+            }
+            for artist in artist_data_all
+        ]
+
+        # Inserción de datos en BigQuery
+        errors = bq_client.insert_rows_json(table_id, rows_to_insert)
+
+        if errors:
+            print(f"Error al insertar los datos en BigQuery: {errors}")
+        else:
+            print(f"Datos insertados correctamente en BigQuery.")
+        
+        return 'Data extracted, saved to Cloud Storage and BigQuery'
+
     except Exception as e:
         print(f"Error en la ejecución de la función: {e}")
         return f"Internal Server Error: {str(e)}", 500
